e2238a0b85e244ad6ca4428389ecdf1e4b7612f7
yuhueng <144983073+yuhueng@users.noreply.github.com>
2024-07-06 23:46:30 +0800

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
