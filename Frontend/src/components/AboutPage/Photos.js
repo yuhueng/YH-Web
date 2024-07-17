75bbdc60c3b603238f90e2be3d680c535f6131a6
yuhueng <144983073+yuhueng@users.noreply.github.com>
2024-07-18 05:06:37 +0800

    Style: Minor UI Changes with Updated Titles for Images

--- Frontend/src/components/AboutPage/Photos.js
+++ Frontend/src/components/AboutPage/Photos.js
@@ -4,140 +4,140 @@ const images = [
       thumb: 'images/nyhpic1.jpg', 
       height: 300,
       width: 300, 
-      // subHtml: "<h4>Photo In: </h4><p>Location: </p>"
+      subHtml: "<h4>Photo Taken In: Iceland</h4>"
     },
     {
       src: 'images/nyhpic2.jpg',
       thumb: 'images/nyhpic2.jpg',
       height: 300,
       width: 300,  
-      // subHtml: "<h4>Photo In: </h4><p>Location: </p>"
+      subHtml: "<h4>Photo Taken In: Iceland</h4>"
     },
     {
       src: 'images/nyhpic3.jpg',
       thumb: 'images/nyhpic3.jpg',
       height: 300,
       width: 300,  
-      // subHtml: "<h4>Photo In: </h4><p>Location: </p>"
+      subHtml: "<h4>Photo Taken In: New Zealand</h4>"
     },
     {
       src: 'images/nyhpic4.jpg',
       thumb: 'images/nyhpic4.jpg',
       height: 300,
       width: 300,  
-      // subHtml: "<h4>Photo In: </h4><p>Location: </p>"
+      subHtml: "<h4>Photo Taken In: New Zealand</h4>"
     },
     {
       src: 'images/nyhpic5.jpg',
       thumb: 'images/nyhpic5.jpg',
       height: 300,
       width: 300,  
-      // subHtml: "<h4>Photo In: </h4><p>Location: </p>"
+      subHtml: "<h4>Photo Taken In: Malaysia</h4>"
     },
     {
       src: 'images/nyhpic6.jpg',
       thumb: 'images/nyhpic6.jpg',
       height: 300,
       width: 300,  
-      // subHtml: "<h4>Photo In: </h4><p>Location: </p>"
+      subHtml: "<h4>Photo Taken In: New Zealand</h4>"
     },
     {
       src: 'images/nyhpic7.jpg',
       thumb: 'images/nyhpic7.jpg',
       height: 300,
       width: 300,  
-      // subHtml: "<h4>Photo In: </h4><p>Location: </p>"
+      subHtml: "<h4>Photo Taken In: New Zealand</h4>"
     },
     {
       src: 'images/nyhpic8.jpg',
       thumb: 'images/nyhpic8.jpg',
       height: 300,
       width: 300,  
-      // subHtml: "<h4>Photo In: </h4><p>Location: </p>"
+      subHtml: "<h4>Photo Taken In: New Zealand</h4>"
     },
     {
       src: 'images/nyhpic9.jpg',
       thumb: 'images/nyhpic9.jpg',
       height: 300,
       width: 300,  
-      // subHtml: "<h4>Photo In: </h4><p>Location: </p>"
+      subHtml: "<h4>Photo In: </h4><p>Location: </p>"
     },
     {
       src: 'images/nyhpic10.jpg',
       thumb: 'images/nyhpic10.jpg',
       height: 300,
       width: 300,  
-      // subHtml: "<h4>Photo In: </h4><p>Location: </p>"
+      subHtml: "<h4>Photo Taken In: New Zealand</h4>"
     },
     {
       src: 'images/nyhpic11.jpg',
       thumb: 'images/nyhpic11.jpg',
       height: 300,
       width: 300,  
-      // subHtml: "<h4>Photo In: </h4><p>Location: </p>"
+      subHtml: "<h4>Photo Taken In: New Zealand</h4>"
     },
     {
         src: 'images/nyhpic12.jpg',
         thumb: 'images/nyhpic12.jpg',
         height: 300,
         width: 300,  
-        // subHtml: "<h4>Photo In: </h4><p>Location: </p>"
+        subHtml: "<h4>Photo Taken In: Singapore</h4> <p>Birthday Celebration!</p>"
       },
       {
         src: 'images/nyhpic13.jpg',
         thumb: 'images/nyhpic13.jpg',
         height: 300,
         width: 300,  
-        // subHtml: "<h4>Photo In: </h4><p>Location: </p>"
+        subHtml: "<h4>Photo Taken In: Singapore</h4>"
       },
       {
         src: 'images/nyhpic14.jpg',
         thumb: 'images/nyhpic14.jpg',
         height: 300,
         width: 300,  
-        // subHtml: "<h4>Photo In: </h4><p>Location: </p>"
+        subHtml: "<h4>Photo Taken In: Iceland</h4>"
       },
       {
         src: 'images/nyhpic15.jpg',
         thumb: 'images/nyhpic15.jpg',
         height: 300,
         width: 300,  
-        // subHtml: "<h4>Photo In: </h4><p>Location: </p>"
+        subHtml: "<h4>Photo Taken In: Iceland</h4>"
       },
       {
         src: 'images/nyhpic16.jpg',
         thumb: 'images/nyhpic16.jpg',
         height: 300,
         width: 300,  
-        // subHtml: "<h4>Photo In: </h4><p>Location: </p>"
+        subHtml: "<h4>Photo Taken In: Iceland</h4>"
       },
       {
         src: 'images/nyhpic17.jpg',
         thumb: 'images/nyhpic17.jpg',
         height: 300,
         width: 300,  
-        // subHtml: "<h4>Photo In: </h4><p>Location: </p>"
+        subHtml: "<h4>Photo Taken In: Iceland</h4>"
       },
       {
         src: 'images/nyhpic18.jpg',
         thumb: 'images/nyhpic18.jpg',
         height: 300,
         width: 300,  
-        // subHtml: "<h4>Photo In: </h4><p>Location: </p>"
+        subHtml: "<h4>Photo Taken In: Iceland</h4>"
       },
       {
         src: 'images/nyhpic19.jpg',
         thumb: 'images/nyhpic19.jpg',
         height: 300,
         width: 300,  
-        // subHtml: "<h4>Photo In: </h4><p>Location: </p>"
+        subHtml: "<h4>Photo Taken In: China</h4>"
       },
       {
         src: 'images/nyhpic20.jpg',
         thumb: 'images/nyhpic20.jpg',
         height: 300,
         width: 300,  
-        // subHtml: "<h4>Photo In: </h4><p>Location: </p>"
+        subHtml: "<h4>Photo Taken In: China</h4>"
       },
   ];
 
